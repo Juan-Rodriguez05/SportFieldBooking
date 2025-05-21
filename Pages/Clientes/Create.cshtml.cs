b15c877d6a5d9be5d98701f238f8293a4bfa7e05
Juan David Ramón <jdramons30@gmail.com>
2025-05-21 01:00:39 -0500

    commit -m"Create Clientes"

---
+++ Pages/Clientes/Create.cshtml.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SportFieldBooking.Pages.Clientes
+{
+    public class CreateModel : PageModel
+    {
+        private readonly SportContext _context;
+        public CreateModel(SportContext context)
+        {
+            _context = context;
+        }
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+        [BindProperty]
+        public Cliente Cliente { get; set; } = default!;
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid || _context.Clientes == null || Cliente == null)
+            {
+                return Page();
+            }
+            _context.Clientes.Add(Cliente);
+            await _context.SaveChangesAsync();
+            return RedirectToPage("./Index");
+        }
+     }
+}
