a43fb3735efdd0a47f96349a49b865740694db0d
Juan-Rodriguez05 <modzjuanes94@gmail.com>
2025-05-21 00:30:46 -0500

    Create Index Page Campos

---
+++ Pages/Campos/Index.cshtml.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SportFieldBooking.Data;
+using SportFieldBooking.Models;
+
+namespace SportFieldBooking.Pages.Campos
+{
+    public class IndexModel : PageModel
+    {
+        private readonly SportContext _context;
+
+        public IndexModel(SportContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Campo> Campos { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Campos != null)
+            {
+                Campos = await _context.Campos.ToListAsync();
+            }
+        }
+    }
+}
