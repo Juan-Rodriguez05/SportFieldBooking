ec8d4a28119906281a43b30c4a622d2aa5eb3aad
Juan David Ramón <jdramons30@gmail.com>
2025-05-21 00:41:52 -0500

    commit -m "Index Clientes"

--- Pages/Clientes/Index.cshtml.cs
+++ Pages/Clientes/Index.cshtml.cs
@@ -5,8 +5,19 @@ namespace SportFieldBooking.Pages.Clientes
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly SportContext _context;
+        public IndexModel(SportContext context)
         {
+            _context = context;
         }
+        public IList<Cliente> Clientes { get; set; } = default!;
+        public async Task OnGetAsync()
+        {
+            if (_context.Clientes != null)
+            {
+                Clientes = await _context.Clientes.ToListAsync();
+            }
+        }
+       
     }
 }
