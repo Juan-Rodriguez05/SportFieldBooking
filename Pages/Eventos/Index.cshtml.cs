12dc52b605894ac154f1af0d414abb94db16ae91
andres-rojas38 <piperojas388@gmail.com>
2025-05-23 08:34:15 -0500

    IndexEventos

---
+++ Pages/Eventos/Index.cshtml.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using SportFieldBooking.Data;
+using SportFieldBooking.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SportFieldBooking.Pages.Eventos
+{
+    public class IndexModel : PageModel
+    {
+       private readonly ApplicationDbContext _context;
+        public IndexModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IList<Evento> Eventos { get; set; } = default!;
+        public async Task OnGetAsync()
+        {
+            if (_context.Eventos != null)
+            {
+                Eventos = await _context.Eventos
+                    .Include(e => e.Campo)
+                    .ToListAsync();
+            }
+        }
+    }
+}
