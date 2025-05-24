99b23d4b7e0873ac51f356cafa09e92772f9329b
andres-rojas38 <piperojas388@gmail.com>
2025-05-24 08:39:57 -0500

    Correccion reservas

--- Pages/Reservas/Create.cshtml.cs
+++ Pages/Reservas/Create.cshtml.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using SportFieldBooking.Data;
 using SportFieldBooking.Models;
 
@@ -18,17 +19,25 @@ namespace SportFieldBooking.Pages.Reservas
         [BindProperty]
         public Reserva Reserva { get; set; } = default!;
 
-        public IActionResult OnGet()
+        public SelectList Campos { get; set; } = default!;
+        public SelectList Clientes { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync()
         {
-            ViewData["IdCampo"] = new SelectList(_context.Campos, "IdCampo", "Nombre");
-            ViewData["IdCliente"] = new SelectList(_context.Clientes, "IdCliente", "Nombre");
+            Campos = new SelectList(await _context.Campos.ToListAsync(), "IdCampo", "Nombre");
+            Clientes = new SelectList(await _context.Clientes.ToListAsync(), "IdCliente", "NombreCompleto");
+
             return Page();
         }
 
         public async Task<IActionResult> OnPostAsync()
         {
             if (!ModelState.IsValid)
+            {
+                Campos = new SelectList(await _context.Campos.ToListAsync(), "IdCampo", "Nombre");
+                Clientes = new SelectList(await _context.Clientes.ToListAsync(), "IdCliente", "NombreCompleto");
                 return Page();
+            }
 
             _context.Reservas.Add(Reserva);
             await _context.SaveChangesAsync();
